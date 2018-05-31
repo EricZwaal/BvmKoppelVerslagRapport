ffcf7d878c7315ada7e85b577865f4cbc0ee02ce
EricZwaal <39811328+EricZwaal@users.noreply.github.com>
2018-06-01 01:11:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,106 @@
-<?php
-echo "Hello WorlD!";
-?>
+<html>
+  <body>
+    <?php
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // get subjectId
+        $subjectId = "";
+        if (isset($_GET['sbid'])) {
+          $subjectId = $_GET['sbid'];
+        }
+        // define variable for language 
+        $language = $languageErr = "";
+        if (empty($_POST["language"])) {
+          $languageErr = "Kies eerst een taal";
+        } else {
+          $language = test_input($_POST["language"]);
+        }
+      }
+      function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+    ?>
+
+    <h2>Koppel een rapport aan dit verslag</h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+      Taal:
+      <input type="radio" name="language" <?php if (isset($language) && $language=="dutch") echo "checked";?> value="dutch">Nederlands
+      <input type="radio" name="language" <?php if (isset($language) && $language=="english") echo "checked";?> value="english">Engels
+      <span class="error"> <?php echo $languageErr;?></span>
+
+      <br><br>
+      <input type="submit" name="button1" value="Koppel het rapport">  
+    </form>
+    <?php
+      if (isset($POST[button1])) {
+        // Hier gaat het gebeuren!
+        // Voor reportconnector:
+        $token = '<token><version>1</version><data>17F8567825A440EEA6B1FDB9F6F6A12E5BA226C040DF93AE1B9D018F89282AB3</data></token>';
+        $encodedToken = base64_encode($token);
+        $url = 'https://50762.afasonlineconnector.nl/ProfitRestServices/reportconnector/FB9AFB3C47411E357DE1108586DBD271?filterfieldids=KnSbjSbId&operatortypes=1&filtervalues=' . $subjectId;
+        $curl = curl_init($url);
+        // Returns the data/output as a string instead of raw data
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        //Set your auth headers
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+          'Authorization: AfasToken ' . $encodedToken
+          ));
+        
+        // get stringified data/output. See CURLOPT_RETURNTRANSFER
+        $result = curl_exec($curl);
+        echo $result;
+        $result = json_decode($result, true));
+        $filedata = $result[filedata];
+        echo $filedata;
+
+        // close curl resource to free up system resources 
+        curl_close($curl)
+
+        $url = 'https://50762.afasonlineconnector.nl/ProfitRestServices/connectors/KnSubject/KnSubjectAttachment'
+        $file = '{
+          "KnSubject": {
+            "Element": {
+              "@SbId": ' . $subjectId . ',
+              "Objects": [
+                {
+                  "KnSubjectAttachment": {
+                    "Element": {
+                      "Fields": {
+                        "FileName": "Report.pdf",
+                        "FileStream": "' . $filedata . '"
+                      }
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        }'
+
+        $curl = curl_init($url);
+        // Returns the data/output as a string instead of raw data
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        // Method
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");   
+        // Set the auth headers
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+          'ContentType: application/json;charset=utf-8',
+          'Authorization: AfasToken ' . $encodedToken
+          ));
+        // Set the body
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $file); 
+        // Make response be put in variable instead of being echoed
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
+        
+        // get stringified data/output. See CURLOPT_RETURNTRANSFER
+        $result = curl_exec($curl);
+        echo $result
+        
+        // close curl resource to free up system resources 
+        curl_close($curl)
+}
+    ?>
+  </body>
+</html>
