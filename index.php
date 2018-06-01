2fd19ac755b65b6292aedc40e4951db479d468fd
EricZwaal <39811328+EricZwaal@users.noreply.github.com>
2018-06-01 21:31:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -58,13 +58,12 @@
         
         // get stringified data/output. See CURLOPT_RETURNTRANSFER
         $result = curl_exec($curl);
-  echo $result;
-//        $result = json_decode($result, true));
-//        $filedata = $result[filedata];
-//  echo $filedata;
+        $result = json_decode($result, true));
+        $filedata = $result[filedata];
+  echo $filedata;
 
         // close curl resource to free up system resources 
-//        curl_close($curl)
+        curl_close($curl)
 
       /*
         $url = 'https://50762.afasonlineconnector.nl/ProfitRestServices/connectors/KnSubject/KnSubjectAttachment'
