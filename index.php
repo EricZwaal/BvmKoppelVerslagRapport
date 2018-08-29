7bfe36a20c28aa4ab43f948668ad702db287db2c
EricZwaal <39811328+EricZwaal@users.noreply.github.com>
2018-08-29 20:50:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,11 +16,11 @@
       } else if (isset($_GET['SbId'])) {
         $subjectId = $_GET['SbId'];
       }
+      $_POST["lngSbId"]=$subjectId;
     ?>
 
     <form name="fMijnFormulier" method="post">
       <br><br>
-      <input type="text"   name=lngSbId   value=.$subjectId/><br />
       <input type="button" name="button1" value="Koppel Nederlands rapport" onclick="Verzend NL" /> <br />  <br>
       <input type="button" name="button2" value="Koppel Engels rapport"     onclick="Verzend EN" /> <br />  
     </form>
