4d45ba826c1063785600c65567cc91f3c8ec6de1
EricZwaal <39811328+EricZwaal@users.noreply.github.com>
2018-08-29 22:03:32 +0200

    Update koppel_verslag.php

--- koppel_verslag.php
+++ koppel_verslag.php
@@ -1,9 +1,9 @@
 <html>
   <body>
     <?php
-      if (isset($_GET['SbId'])) {
-        $subjectId = $_GET['SbId'];
-      }
+      session_start();
+      $subjectId=$_SESSION['subjectId'];
+
       if($_SERVER['REQUEST_METHOD']=="POST"){
         $subjectId=$_POST["lngSbId"];
         switch($_POST["button"]) {
