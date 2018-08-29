99d34e3cb3add75ae9966ca3dcbf3de1455e7fec
EricZwaal <39811328+EricZwaal@users.noreply.github.com>
2018-08-29 20:38:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
   <head>
     <script language="VBScript">
-      sub Verzend(actie)
-        fMijnFormulier.action="koppel_verslag.php?taal=" & taal
+      sub Verzend(taal, sbid)
+        fMijnFormulier.action="koppel_verslag.php?taal=" & taal & "&subjectId=" & sbid
         fMijnFormulier.submit
       end sub
     </script>
@@ -20,8 +20,8 @@
 
     <form name="fMijnFormulier" method="post">
       <br><br>
-      <input type="button" name="button1" value="Koppel Nederlands rapport" onclick="Verzend NL" /> <br />  <br>
-      <input type="button" name="button2" value="Koppel Engels rapport"     onclick="Verzend EN" /> <br />  
+      <input type="button" name="button1" value="Koppel Nederlands rapport" onclick="Verzend NL, " . $subjectId /> <br />  <br>
+      <input type="button" name="button2" value="Koppel Engels rapport"     onclick="Verzend EN, " . $subjectId /> <br />  
     </form>
   </body>
 </html>
